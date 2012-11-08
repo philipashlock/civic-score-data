6b3b5a691525f089dd647e9a7fb843586a2c31b7
Philip Ashlock <phil@civicagency.org>
2012-11-08 18:02:02 -0500

    Adding register link

--- application/views/header_inc_view.php
+++ application/views/header_inc_view.php
@@ -7,7 +7,9 @@
       <a href="/"><div id="logo" class="span6"><h1>School Status Finder</h1></div></a>
       <div class="span3">
         <a href="#aboutModal" role="button" class="btn btn-info" data-toggle="modal">About</a>
-        <a href="/api/" role="button" class="btn btn-info">API Docs</a>
+        <a href="/api/" role="button" class="btn btn-info">API</a>
+        <a href="/status/register" role="button" class="btn btn-info">Register</a>
+
 
       </div>
       </div>
